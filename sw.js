b61509d3d2d49b89d87435471de2d874b087361e
damiennikon <damiennikon@gmail.com>
2026-06-07 21:12:13 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,3 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
     return self.clients.claim();
 });
-
-self.addEventListener('fetch', (e) => {
-    // We don't need offline caching for this app, so we just let requests pass through normally.
-});
